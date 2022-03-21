654a888ef92e891e3a84105d19e9489e00fa1d96
Shannon Bulimo <shannonbulimo@gmail.com>
2022-03-21 03:55:10 +0300

    functions

--- script.js
+++ script.js
@@ -43,3 +43,10 @@ $(document).ready(function() {
   $(".btn.yes").hide();
   $(".btn.no").hide();
   $(".additional-info h4").hide();
+  $('.btn.order').click(function() {
+    var sizeOfPizza = $(".size option:selected").val();
+    var toppingsOfPizza = $(".toppings option:selected").val();
+    var crustOfPizza = $(".crust option:selected").val();
+    var total = parseInt(sizeOfPizza) + parseInt(toppingsOfPizza) + parseInt(crustOfPizza);
+    var order = 1;
+    var grandTotal = 0;
